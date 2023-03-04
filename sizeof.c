41ba3d6b122660a6c8ceff06bbfa3a6bd089015e
Thor Long <thorlong17@outlook.com>
2023-03-04 16:19:44 -0500

    3/4/2023

--- sizeof.c
+++ sizeof.c
@@ -2,10 +2,44 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(){
-    void *vptr = malloc(1024); //change both data types to cast
-    fprintf(stdout,"sizeof(vptr) = %ld\n", sizeof(vptr)); //pointer 64 bit program, 8 bytes per pointer
-    fprintf(stdout,"sizeof(*vptr) = %ld\n", sizeof(*vptr)); //what is the size of what it's pointing to, currently void, defaults to char 1 byte
-    fprintf(stdout,"sizeof(vptr) = %ld\n", strlen(vptr));
-    return 0;
+
+void yomama(int *ptr, int x);
+
+int main() {
+  
+  void *vptr = malloc(1024);
+  fprintf(stdout, "sizeof(vptr) = %ld\n", sizeof(vptr));
+  fprintf(stdout, "sizeof(*vptr) = %ld\n", sizeof(*vptr));  /* same as vptr[0] */
+
+  char *cptr = (char *)malloc(1024);
+  for (int i = 0; i < 1024; i++) {
+    cptr[i] = 'z';
+    fprintf(stdout, "%c", cptr[i]);
+  }
+
+  cptr[1024] = 'a';
+  fprintf(stdout, "sizeof(cptr) = %ld\n", sizeof(cptr));
+  fprintf(stdout, "strlen(cptr) = %ld\n", strlen(cptr));
+  
+  int *iptr = (int *) malloc(256 /**/* sizeof(int)); //Allocates for a dynamic size of integer, or structure that holds integers etc.
+  fprintf(stdout, "sizeof(iptr) = %ld\n", sizeof(iptr));
+  fprintf(stdout, "sizeof(*iptr) = %ld\n", sizeof(*iptr));
+  //instead of defaulting to 4, expect 19 to print size of integer, 8. Line 20 will return 4 because it is defined as integer.
+  //if malloc returns nonzero, we know we're pointing to a pointer of that size unless otherwise, or null terminated.
+  iptr[256] = -1; 
+
+  int dork = 391;
+  //how do we set a pointer, declare and define, so we can access dork in memory (without referring to dork)
+  int *ptrDork = &dork;
+
+  yomama(ptrDork, 3); //call to function, need prototype 
+
+  printf("Dork: %d\n",dork);
+  printf("Dork pointer: %ls\n", ptrDork); //ls like the command..?
+
+  return 0;
+}
+
+void yomama(int *ptr, int x){
+  *ptr = 2 * *ptr + x;
 }
